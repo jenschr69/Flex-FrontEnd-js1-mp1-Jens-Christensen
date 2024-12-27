3a5d6e9ff5bdcf32149fb5509af63ba826a3bd35
Jens Christensen - Glodexa <jens@glodexa.com>
2024-12-27 21:17:48 +0100

    Adding initial files

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+// Start with adding h1-tag with id to the dom
+// HTML and text to be added: <h1>Flex-FrontEnd-js1-mp1-JensChristensen</h1>
+
+// Add 5 rows using div tags
+// In each row the text "Rad" + row number should be added
+// For each of the 5 rows the size should increase
+// Row 1: 0.6rem, Row 2: 0.8 rem, Row 3: 1.2 rem, Row 4: 1.6 rem, Row 5: 1.8 rem
+
